57992a4f8c4fb1ba7e1597259e50bd1e56103916
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2020-12-27 09:29:27 -0500

    varibale
    changed varibale types

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-var shapeChoice = document.getElementById("shape");
-var colorChoice = document.getElementById("color");
-var sizeChoice = document.getElementById("size");
-var submitButton = document.getElementById("submit");
+const shapeChoice = document.getElementById("shape");
+const colorChoice = document.getElementById("color");
+const sizeChoice = document.getElementById("size");
+const submitButton = document.getElementById("submit");
 
 function createShape() {
     console.log(shapeChoice)
 }
 
 submitButton.addEventListener("click", createShape());
-
