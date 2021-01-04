c0e8e65ac4e60d109e191dce531befaffe0ff6d4
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-01-04 11:37:19 -0500

    local
    added local storage that saves properties inot objects. then creates line of code that recreates the objects when restarting the server. could add a save button. aslo should add a delete button.
    also fixed issue where margins messed with shapes that needed 2 divs.

--- main.js
+++ main.js
@@ -4,13 +4,29 @@ const colorChoice = document.getElementById("color");
 const sizeChoice = document.getElementById("size");
 const submitButton = document.getElementById("submit");
 const shapeContainer = document.getElementById("shapeContainer");
+var shapes = JSON.parse(localStorage.getItem("madeShapes")) || [];
 import { create } from "./create.js";
 
-
+console.log(shapes);
+// creating all previous shapes
+if (shapes.lenth >= 1) {
+    shapes.forEach(shape => {
+        const newShape = create(shape.form, shape.color, shape.size);
+        shapeContainer.appendChild(newShape);
+    });
+}
 submitButton.addEventListener("click", function() {
     const chosenShape = shapeChoice.value;
     const chosenColor = colorChoice.value;
     const chosenSize = sizeChoice.value;
     const newShape = create(chosenShape, chosenColor, chosenSize);
     shapeContainer.appendChild(newShape);
+    const shapeSaved = {
+        form: chosenShape,
+        color: chosenColor,
+        size: chosenSize,
+    };
+    shapes.push(shapeSaved);
+    console.log(shapes);
+    localStorage.setItem("madeShapes", JSON.stringify(shapes));
 });
