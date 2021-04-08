0137be1c07e8b667ae317077a0b81b16bab0c3ac
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-04-08 09:22:42 -0400

    Revert "more string literals, imports, spelling"
    This reverts commit 3592d5723884da5399217e0793eb27097148c2e2.

--- code/create.js
+++ code/create.js
@@ -1,5 +1,4 @@
-
-export function create(shape, color, size) {
+ export function create(shape, color, size) {
     // creating div
     const newShape = document.createElement("div");
     // secondary color
@@ -8,7 +7,7 @@ export function create(shape, color, size) {
     const halfSize = size / 2;
     const twiceSize = size * 2;
     const threeTenthsSize = size * 3 / 10;
-    // setting sizes to string 
+    // setting size to string 
     const sizeString = `${size * 50}px`;
     const twiceSizeString = `${twiceSize * 50}px`;
     const halfSizeString = `${halfSize * 50}px`;
@@ -22,8 +21,8 @@ export function create(shape, color, size) {
     // molding the shape
     switch(shape) {
         case "Square":
-            debugger;
-            createSquare(newShape, sizeString);
+            newShape.style.width = sizeString;
+            newShape.style.height = sizeString;
             break;
         case "Rectangle":
             newShape.style.width = twiceSizeString;
@@ -44,7 +43,7 @@ export function create(shape, color, size) {
             newShape.style.height = 0;
             newShape.style.borderLeft = `${halfSizeString} solid transparent`;
             newShape.style.borderRight = `${halfSizeString} solid transparent`;
-            newShape.style.borderBottom = `${sizeString} solid ${color}`;
+            newShape.style.borderBottom = `${sizeString} solid color`;
             newShape.style.backgroundColor = secondaryColor;
             break;
         case "Star":
@@ -52,14 +51,14 @@ export function create(shape, color, size) {
             newShape.style.height = 0;
             newShape.style.borderLeft = `${halfSizeString} solid transparent`;
             newShape.style.borderRight = `${halfSizeString} solid transparent`;
-            newShape.style.borderBottom = `${sizeString} solid ${color}`;
+            newShape.style.borderBottom = `${sizeString} solid color`;
             newShape.style.backgroundColor = secondaryColor;
             newShape.style.position = "relative";
             Shape2.style.width = 0;
             Shape2.style.height = 0;
             Shape2.style.borderLeft = `${halfSizeString} solid transparent`;
             Shape2.style.borderRight = `${halfSizeString} solid transparent`;
-            Shape2.style.borderTop = `${sizeString} solid ${color}`;
+            Shape2.style.borderTop = `${sizeString} solid color`;
             Shape2.style.backgroundColor = "transparent";
             Shape2.style.position = "absolute";
             Shape2.style.left = `-${halfSizeString}`;
@@ -84,6 +83,34 @@ export function create(shape, color, size) {
             Shape2.style.top = halfSizeString;
             newShape.append(Shape2);
             break;
+        // case "Pentagon":
+        //     newShape.style.width = sizeString;
+        //     newShape.style.height = 0;
+        //     newShape.style.boxSizing = "content-box";
+        //     newShape.style.borderWidth = size + halfSize + "0";
+        //     newShape.style.borderStyle = "solid";
+        //     newShape.style.borderColor = color + " transparent";
+        //     newShape.style.backgroundColor = secondaryColor;
+        //     newShape.style.position = "relative";
+        //     const Shape2 = document.createElement("div");
+        //     Shape2.style.width = 0;
+        //     Shape2.style.height = 0;
+        //     Shape2.style.borderLeft = halfSize + " solid transparent";
+        //     Shape2.style.borderRight = halfSize + " solid transparent";
+        //     Shape2.style.borderTop = size + " solid " + color;
+        //     Shape2.style.backgroundColor = "transparent";
+        //     Shape2.style.position = "absolute";
+        //     Shape2.style.left = "-" + halfSize;
+        //     Shape2.style.top = "-" + twiceSize;
+        //     Shape2.style.borderWidth = "0 " + size + threeTenthsSize;
+        //     Shape2.style.borderStyle = "solid";
+        //     Shape2.style.borderColor = "transparent transparent " + color;
+        //     newShape.append(Shape2);
+        //     break;
+        // case "Heart":
+        //     newShape.style.width = size;
+        //     newShape.style.height = size;
+        //     break;
         default:  break;
     }
     return newShape;
